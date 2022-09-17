dc30834f6021d810674bd19c072d7abe8e570f4e
Paulo Henrique da Rosa <101262901+paulinhodarosa@users.noreply.github.com>
2022-09-16 23:51:27 -0300

    poo atividades

--- at5/Cachorro.java
+++ at5/Cachorro.java
@@ -1,9 +1,3 @@
-//Atividade 5
-/* IFPR Campus Cascavel
-Aluno: Paulo Henrique da Rosa
-Professor: Nelson Bellicanta
-Disciplina: Programação Orientada à Objetos (POO)
-*/
 public class Cachorro extends Animal{
     // private String acao;
     public Cachorro(String nome, int idade){
